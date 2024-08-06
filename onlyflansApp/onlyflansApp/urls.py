fa9921ac901c872b7e5e2396767113ce802755f1
Daniel Veloso <dvelosocastillo@gmail.com>
2024-08-06 17:11:33 -0400

    se agrega web app y se agrega index bienvenido y acerca

--- onlyflansApp/onlyflansApp/urls.py
+++ onlyflansApp/onlyflansApp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web.views import bienvenido, acerca, indice
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indice, name='indice'),
+    path('acerca', acerca, name='acerca'),
+    path('bienvenido', bienvenido, name='bienvenido'),
 ]
